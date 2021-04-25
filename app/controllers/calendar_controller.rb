1bfed1bf434d9d2ffde77e83827bda868a7322ea
Taylor <tjaroguetech@gmail.com>
2021-04-24 21:46:36 -0400

    Added Faker gem for seed data and tested adding Users to the Calendar controller to display them on the calendar dates.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,6 +1,6 @@
 class CalendarController < ApplicationController
   def show
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
-    @employee = { "name" => "Allegra", "workday" => [Date.yesterday, Date.today, Date.tomorrow]}
+    @employees = User.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+    User.create(email: Faker::Internet.email, first_name: Faker::FunnyName.name.split(" ")[0], last_name: Faker::FunnyName.name.split(" ")[1], encrypted_password: "password", job_title: "Optical Admin")
+end
