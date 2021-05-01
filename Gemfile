2022ffcded21334ddd3136b947c85ae8217567fc
tjaRoxasXIII <tjaroguetech@gmail.com>
2021-05-01 02:22:00 -0400

    Working out the multiple user schedule form.  Currently able to create a 'Workday' for each selected employee

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'devise'
 
 gem 'faker'
 
+gem 'pry'
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/controllers/workdays_controller.rb
+++ app/controllers/workdays_controller.rb
@@ -1,2 +1,22 @@
+require 'pry'
 class WorkdaysController < ApplicationController
+    def new
+        @workday = Workday.new
+    end
+
+    def create
+        workday_parameters["id"].select {|a| a != '0'}.each do |user|
+            @workday = Workday.new(workday_parameters)
+            @workday.user_id = user.to_i
+            @workday.save
+        end
+        link_to 'localhost:3000'
+
+    end
+
+    private
+
+    def workday_parameters
+        params.require(:workday).permit(:date, :start_hour, :start_min, :end_hour, :end_min, :id => [])
+    end
 end
