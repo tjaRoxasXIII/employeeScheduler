ffc1aa76f9965b84fd8b8b6ce368073755b8eae5
Taylor <tjaroguetech@gmail.com>
2021-05-15 19:30:40 -0400

    Re-working logic for faster loading

--- app/controllers/workdays_controller.rb
+++ app/controllers/workdays_controller.rb
@@ -6,13 +6,14 @@ class WorkdaysController < ApplicationController
 
     def create
         workday_parameters["id"].select {|a| a != '0'}.each do |user|
-            @workday = Workday.find_or_create_by(:date => workday_parameters["date"], :user_id => user)
-            @workday.start_hour = workday_parameters["start_hour"]
-            @workday.start_min = workday_parameters["start_min"]
-            @workday.end_hour = workday_parameters["end_hour"]
-            @workday.end_min = workday_parameters["end_min"]
-            @workday.user_id = user.to_i
-            @workday.save
+            @workday = Workday.find_or_create_by(:date => workday_parameters["date"],
+                :user_id => user.to_i, 
+                :start_hour => workday_parameters["start_hour"],
+                :start_min => workday_parameters["start_min"],
+                :start_am_or_pm => workday_parameters["start_am_or_pm"],
+                :end_hour => workday_parameters["end_hour"],
+                :end_min => workday_parameters["end_min"],
+                :end_am_or_pm => workday_parameters["end_am_or_pm"])
         end
         redirect_to '/'
 
